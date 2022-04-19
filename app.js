dce4d1b14586a638082d2c3301dbbfc0f77cf3ae
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-04-19 13:37:35 +0100

    adding the toggle from the javascript

---
+++ app.js
@@ -0,0 +1,9 @@
+let plus = document.querySelector(".span");
+let panelContent = document.querySelector(".panel");
+
+span.addEventListener("click", () => {
+  if (span === "&#43") {
+    panel.style.height = "auto";
+    span.style.display = "&#150";
+  }
+});
