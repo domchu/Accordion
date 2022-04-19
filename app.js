e47262b73970ed8dbb94119dbff469367f49b788
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-04-19 22:21:48 +0100

    adding more design and styles

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 let fas = document.querySelectorAll(".fas");
 let tab = document.querySelectorAll(".tab");
 let contentText = document.querySelectorAll(".content-text");
-// document.querySelectorAll
 
 for (let index = 0; index < tab.length; index++) {
   tab[index].addEventListener("click", () => {
